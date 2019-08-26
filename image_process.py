6db615c294afe53453ce7c0e08ab63ef11154f74
vivasoft <vargaspintoadriana@gmail.com>
2019-08-25 19:58:55 -0500

    save process image /media/process

--- image_process.py
+++ image_process.py
@@ -9,26 +9,28 @@ url = 'http://127.0.0.1:8000/disenos/'
 response = requests.get(url)
 designs = json.loads(response.text)
 
-print(designs)
-
+# Se recorre el json con las imagenes sin procesar
 for i in designs:
 
     desired_size = 800
 
     img = Image.open('../media/{}'.format(i['original_file']))
-    print(img.size)
 
     # Se ajusta tamaño de la imagen
     old_size = img.size
     ratio = float(desired_size)/max(old_size)
     new_size = tuple([int(x*ratio) for x in old_size])
     img = img.resize(new_size, Image.ANTIALIAS)
-    print(img.size)
 
     # se comenta la imagen con el nombre del diseñador y fecha
     draw = ImageDraw.Draw(img)
-    draw.text((10, 10), 'Diseñador: {} {} - Fecha: {}'.format(i['designer_name'], i['designer_last_name']
+    draw.text((10, 10), 'Diseñador: {} {}\nFecha: {}'.format(i['designer_name'], i['designer_last_name']
                                                               , i['created_date']), fill="white")
 
+    # Obtener nombre de archivo
+    file = '{}'.format(i['original_file'])
+    position = file.index('.')
+    filename = file[9:position]
+
     # Se guarda nueva imagen
-    img.save('../media/process/publicidad.png')
+    img.save('../media/process/' + filename + '.png')
