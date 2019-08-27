79bdfce301b71c885fcfadb5271aa7e010f7d6aa
vivasoft <vargaspintoadriana@gmail.com>
2019-08-27 17:57:35 -0500

    Add while true (5 sec) + log

--- image_process.py
+++ image_process.py
@@ -1,42 +1,57 @@
+from datetime import datetime
+import time
+
 import requests
 from PIL import Image, ImageDraw
 from rest_framework.utils import json
 
-url = 'http://127.0.0.1:8000/disenos/'
-url_update = 'http://127.0.0.1:8000/diseno-act/'
+# Se ejecuta cada 5 segundos indefinidamente
+while True:
 
-# Consulta las imagenes sin procesar
-response = requests.get(url)
-designs = json.loads(response.text)
+    url = 'http://127.0.0.1:8000/disenos/'
+    url_update = 'http://127.0.0.1:8000/diseno-act/'
 
-# Se recorre el json con las imagenes sin procesar
-for i in designs:
+    # Consulta las imagenes sin procesar
+    response = requests.get(url)
+    designs = json.loads(response.text)
 
-    desired_size = 800
+    # Se recorre el json con las imagenes sin procesar
+    for i in designs:
 
-    img = Image.open('../media/{}'.format(i['original_file']))
+        desired_size = 800
 
-    # Se ajusta tamaño de la imagen
-    old_size = img.size
-    ratio = float(desired_size)/max(old_size)
-    new_size = tuple([int(x*ratio) for x in old_size])
-    img = img.resize(new_size, Image.ANTIALIAS)
+        img = Image.open('../media/{}'.format(i['original_file']))
 
-    width, height = img.size
+        # Se ajusta tamaño de la imagen
+        old_size = img.size
+        ratio = float(desired_size)/max(old_size)
+        new_size = tuple([int(x*ratio) for x in old_size])
+        img = img.resize(new_size, Image.ANTIALIAS)
 
-    # se comenta la imagen con el nombre del diseñador y fecha
-    draw = ImageDraw.Draw(img)
-    draw.text((20, height-30), 'Diseñador: {} {}\nFecha: {}'.format(i['designer_name'], i['designer_last_name']
-                                                              , i['created_date']), fill="white")
+        width, height = img.size
 
-    # Obtener nombre de archivo
-    file = '{}'.format(i['original_file'])
-    position = file.index('.')
-    filename = file[9:position]
+        # se comenta la imagen con el nombre del diseñador y fecha
+        draw = ImageDraw.Draw(img)
+        draw.text((20, height-30), 'Diseñador: {} {}\nFecha: {}'.format(i['designer_name'], i['designer_last_name']
+                                                                  , i['created_date']), fill="white")
 
-    # Se guarda nueva imagen
-    img.save('../media/process/' + filename + '.png')
+        # Obtener nombre de archivo
+        file = '{}'.format(i['original_file'])
+        position = file.index('.')
+        filename = file[9:position]
 
-    # Se actualiza estado y ruta del diseño procesado
-    data = {"original_file": '{}'.format(i['original_file']), "process_file": 'process/' + filename + '.png'}
-    r = requests.put(url_update, data=json.dumps(data), timeout=10)
+        # Se guarda nueva imagen
+        img.save('../media/process/' + filename + '.png')
+
+        # Se actualiza estado y ruta del diseño procesado
+        data = {"original_file": '{}'.format(i['original_file']), "process_file": 'process/' + filename + '.png'}
+        r = requests.put(url_update, data=json.dumps(data), timeout=10)
+
+        # registro en log de eventos
+        f = open("../logs/log", "a+")
+        now = datetime.now()
+        f.write("Hora: %a " % now.strftime("%d/%m/%Y %H:%M:%S") + "-> Process " + '{}'.format(i['original_file']) +
+                " to " + 'process/' + filename + '.png \r\n')
+        f.close()
+
+        time.sleep(5)
