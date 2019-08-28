5c26d364a5f6246187e730c1a59fc129b30ec39f
Nicolas Lema <n.lema@uniandes.edu.co>
2019-08-27 19:30:31 -0500

    Se elimina la carpeta logs y se quita el while true del scriot de procesamiento

--- image_process.py
+++ image_process.py
@@ -1,57 +1,58 @@
-from datetime import datetime
 import time
-
+import os
+import django
+from datetime import datetime
 import requests
 from PIL import Image, ImageDraw
 from rest_framework.utils import json
+from django.conf import settings
 
-# Se ejecuta cada 5 segundos indefinidamente
-while True:
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_1.settings")
+django.setup()
 
-    url = 'http://127.0.0.1:8000/disenos/'
-    url_update = 'http://127.0.0.1:8000/diseno-act/'
+url = 'http://127.0.0.1:8000/disenos/'
+url_update = 'http://127.0.0.1:8000/diseno-act/'
 
-    # Consulta las imagenes sin procesar
-    response = requests.get(url)
-    designs = json.loads(response.text)
+# Consulta las imagenes sin procesar
+response = requests.get(url)
+designs = json.loads(response.text)
 
-    # Se recorre el json con las imagenes sin procesar
-    for i in designs:
+# Se recorre el json con las imagenes sin procesar
+for i in designs:
+    desired_size = 800
 
-        desired_size = 800
+    img = Image.open(settings.MEDIA_ROOT + '/{}'.format(i['original_file']))
 
-        img = Image.open('../media/{}'.format(i['original_file']))
+    # Se ajusta tamaño de la imagen
+    old_size = img.size
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    img = img.resize(new_size, Image.ANTIALIAS)
 
-        # Se ajusta tamaño de la imagen
-        old_size = img.size
-        ratio = float(desired_size)/max(old_size)
-        new_size = tuple([int(x*ratio) for x in old_size])
-        img = img.resize(new_size, Image.ANTIALIAS)
+    width, height = img.size
 
-        width, height = img.size
+    # se comenta la imagen con el nombre del diseñador y fecha
+    draw = ImageDraw.Draw(img)
+    draw.text((20, height - 30), 'Diseñador: {} {}\nFecha: {}'.format(i['designer_name'], i['designer_last_name']
+                                                                      , i['created_date']), fill="white")
 
-        # se comenta la imagen con el nombre del diseñador y fecha
-        draw = ImageDraw.Draw(img)
-        draw.text((20, height-30), 'Diseñador: {} {}\nFecha: {}'.format(i['designer_name'], i['designer_last_name']
-                                                                  , i['created_date']), fill="white")
+    # Obtener nombre de archivo
+    file = '{}'.format(i['original_file'])
+    position = file.index('.')
+    filename = file[9:position]
 
-        # Obtener nombre de archivo
-        file = '{}'.format(i['original_file'])
-        position = file.index('.')
-        filename = file[9:position]
+    # Se guarda nueva imagen
+    img.save(settings.MEDIA_ROOT + '/process/' + filename + '.png')
 
-        # Se guarda nueva imagen
-        img.save('../media/process/' + filename + '.png')
+    # Se actualiza estado y ruta del diseño procesado
+    data = {"original_file": '{}'.format(i['original_file']), "process_file": 'process/' + filename + '.png'}
+    r = requests.put(url_update, data=json.dumps(data), timeout=10)
 
-        # Se actualiza estado y ruta del diseño procesado
-        data = {"original_file": '{}'.format(i['original_file']), "process_file": 'process/' + filename + '.png'}
-        r = requests.put(url_update, data=json.dumps(data), timeout=10)
+    # registro en log de eventos
+    f = open(settings.BASE_DIR + "/logs/log", "a+")
+    now = datetime.now()
+    f.write("Hora: %a " % now.strftime("%d/%m/%Y %H:%M:%S") + "-> Process " + '{}'.format(i['original_file']) +
+            " to " + 'process/' + filename + '.png \r\n')
+    f.close()
 
-        # registro en log de eventos
-        f = open("../logs/log", "a+")
-        now = datetime.now()
-        f.write("Hora: %a " % now.strftime("%d/%m/%Y %H:%M:%S") + "-> Process " + '{}'.format(i['original_file']) +
-                " to " + 'process/' + filename + '.png \r\n')
-        f.close()
-
-        time.sleep(5)
+    time.sleep(5)
