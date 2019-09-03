e78a774e1612cffa2c82fb6e6a44233584d57f57
Nicolas Lema <n.lema@uniandes.edu.co>
2019-09-02 19:47:49 -0500

    Se agrega control de acceso a subir las imagenes

--- designs/views.py
+++ designs/views.py
@@ -243,9 +243,13 @@ def update_url(request):
 
 
 def upload_design(request):
-    project = Project.objects.filter(name='Dia de la mujer')
-    state = State.objects.get(id=1)
-    design = Design(value=1, designer_email='1@1.com', designer_last_name='apellido', designer_name='nombre',
-                    original_file='original/imagen-consciencia.jpg', state=state, project=project[0])
-    design.save()
+    try:
+        project = Project.objects.filter(name='Dia de la mujer')
+        state = State.objects.get(id=1)
+        design = Design(value=1, designer_email='1@1.com', designer_last_name='apellido', designer_name='nombre',
+                        original_file='original/imagen-consciencia.jpg', state=state, project=project[0])
+        design.save()
+        return HttpResponse(status=204)
+    except Exception:
+        return HttpResponse(status=500)
 
