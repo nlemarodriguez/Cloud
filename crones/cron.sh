0bb8a1211ca6a9c010dd5d0a376de7c0d225ee8f
EC2 Default User <ec2-user@ip-172-31-26-101.ec2.internal>
2019-09-12 02:54:52 +0000

    se modifican rutas del cron

--- crones/cron.sh
+++ crones/cron.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-export MAIN_URL="http://172.24.42.49:8081/"
-cd /home/SIS/dn.lecca/Grupo01
-/usr/bin/python3 /home/SIS/dn.lecca/Grupo01/image_process.py
+cd /home/ec2-user/Grupo01_env/bin/
+source activate
+source /home/ec2-user/var
+cd /home/ec2-user/Grupo01
+python3 /home/ec2-user/Grupo01/image_process.py
--- crones/programar.py
+++ crones/programar.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
-my_cron = CronTab(user='dn.lecca')
+my_cron = CronTab(user='ec2-user')
 print('prueba')
-job = my_cron.new(command='/home/SIS/dn.lecca/crones/cron.sh >> /home/SIS/dn.lecca/crones/myscript.log 2>&1', comment='1')
+job = my_cron.new(command='/home/ec2-user/Grupo01/crones/cron.sh >> /home/ec2-user/Grupo01/crones/myscript.log 2>&1', comment='1')
 job.minute.every(1)
  
 my_cron.write()
