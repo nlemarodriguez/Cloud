734883030912d8c058f287608ddd6ee1f1156f16
Nicolas Lema <n.lema@uniandes.edu.co>
2019-09-15 20:05:01 -0500

    Se eliminan archivos

--- crones/programar.py
+++
@@ -1,7 +0,0 @@
-from crontab import CronTab
-my_cron = CronTab(user='dec2-user')
-print('prueba')
-job = my_cron.new(command='/home/ec2-user/Grupo01/crones/cron.sh >> /home/ec2-user/Grupo01/crones/myscript.log 2>&1', comment='1')
-job.minute.every(1)
- 
-my_cron.write()
