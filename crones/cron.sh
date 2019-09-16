0ecfb8df7d91175aaea1c4097ea8a1054ddbc8b5
EC2 Default User <ec2-user@ip-172-31-19-64.ec2.internal>
2019-09-16 01:02:27 +0000

    crones

--- crones/cron.sh
+++ crones/cron.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-export MAIN_URL="http://172.24.42.49:8081/"
-cd /home/SIS/dn.lecca/Grupo01
-/usr/bin/python3 /home/SIS/dn.lecca/Grupo01/image_process.py
+cd /home/ec2-user/venv_Grupo01/bin
+source activate
+export MAIN_URL="http://54.174.167.225:8080"
+export BD_DESIGN="designmatch"
+export BD_DESIGN_HOST="database-1.cjy24azleai9.us-east-1.rds.amazonaws.com"
+export BD_DESIGN_PASSWORD="Hencho00"
+export BD_DESIGN_USUARIO="admin_postgres"
+export EMAIL_DESIGN_KEY="AKIASIH5IUK47FQZOPL3"
+export EMAIL_DESIGN_SECRET_KEY="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"
+export EMAIL_DESIGN_USER="n.lema@uniandes.edu.co"
+cd /home/ec2-user/Grupo01
+python3 /home/ec2-user/Grupo01/image_process.py
