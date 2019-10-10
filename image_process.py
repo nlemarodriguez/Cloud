b004aec6de8f37271ab07333026f3637f85676db
Nicolas Lema <n.lema@uniandes.edu.co>
2019-10-09 22:41:41 -0500

    Procesar una imagen por vez

--- image_process.py
+++ image_process.py
@@ -11,32 +11,34 @@ django.setup()
 from designs.models import Company, Project, Design, State
 
 design = Design.objects.filter(process_file='')[0]
-desired_size = 800
-img = Image.open(settings.MEDIA_ROOT + '/{}'.format(design.original_file))
+while design:
+    desired_size = 800
+    img = Image.open(settings.MEDIA_ROOT + '/{}'.format(design.original_file))
 
-# Se ajusta tamaño de la imagen
-old_size = img.size
-ratio = float(desired_size) / max(old_size)
-new_size = tuple([int(x * ratio) for x in old_size])
-img = img.resize(new_size, Image.ANTIALIAS)
+    # Se ajusta tamaño de la imagen
+    old_size = img.size
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    img = img.resize(new_size, Image.ANTIALIAS)
 
-width, height = img.size
+    width, height = img.size
 
-# se comenta la imagen con el nombre del diseñador y fecha
-draw = ImageDraw.Draw(img)
-draw.text((20, height - 30), 'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
-                                                                  , design.created_date), fill="white")
+    # se comenta la imagen con el nombre del diseñador y fecha
+    draw = ImageDraw.Draw(img)
+    draw.text((20, height - 30), 'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
+                                                                      , design.created_date), fill="white")
 
-# Obtener nombre de archivo
-file = '{}'.format(design.original_file)
-position = file.index('.')
-filename = file[9:position]
-print(filename)
-# Se guarda nueva imagen
-img.save(settings.MEDIA_ROOT + '/process/' + filename + '.png')
+    # Obtener nombre de archivo
+    file = '{}'.format(design.original_file)
+    position = file.index('.')
+    filename = file[9:position]
+    print(filename)
+    # Se guarda nueva imagen
+    img.save(settings.MEDIA_ROOT + '/process/' + filename + '.png')
 
-status, created = State.objects.get_or_create(name='Disponible')
+    status, created = State.objects.get_or_create(name='Disponible')
 
-design.state = status
-design.process_file = 'process/' + filename + '.png'
-design.save()
+    design.process_file = 'process/' + filename + '.png'
+    design.state = status
+    design.save()
+    design = Design.objects.filter(process_file='')[0]
