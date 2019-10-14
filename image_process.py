c37e28a7ba9ab65588a81c280925e779578b4291
Nicolas Lema <n.lema@uniandes.edu.co>
2019-10-13 21:22:02 -0500

    Se ajusta proceso de colas

--- image_process.py
+++ image_process.py
@@ -10,6 +10,7 @@ django.setup()
 from django.core.mail import send_mail
 from designs.models import Company, Project, Design, State
 import boto3
+from botocore.exceptions import ClientError
 
 sqs = boto3.client('sqs', region_name='us-east-1')
 queue_url = 'https://sqs.us-east-1.amazonaws.com/155149968057/modeloD-Cola'
@@ -17,64 +18,71 @@ queue_url = 'https://sqs.us-east-1.amazonaws.com/155149968057/modeloD-Cola'
 
 while True:
 
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
-    )
+    try:
+        # Receive message from SQS queue
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+    except ClientError as e:
+        response = None
 
-    message = response['Messages'][0]
-    receipt_handle = message['ReceiptHandle']
+    if response is not None:
+        try:
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
 
-    id = message['MessageAttributes']['Id']['StringValue']
-    design = Design.objects.get(id=int(id))
+            id = message['MessageAttributes']['Id']['StringValue']
+            design = Design.objects.get(id=int(id))
 
-    desired_size = 800
-    img = Image.open(design.original_file)
+            desired_size = 800
+            img = Image.open(design.original_file)
 
-    # Se ajusta tamaño de la imagen
-    old_size = img.size
-    ratio = float(desired_size) / max(old_size)
-    new_size = tuple([int(x * ratio) for x in old_size])
-    img = img.resize(new_size, Image.ANTIALIAS)
+            # Se ajusta tamaño de la imagen
+            old_size = img.size
+            ratio = float(desired_size) / max(old_size)
+            new_size = tuple([int(x * ratio) for x in old_size])
+            img = img.resize(new_size, Image.ANTIALIAS)
 
-    width, height = img.size
+            width, height = img.size
 
-    # se comenta la imagen con el nombre del diseñador y fecha
-    draw = ImageDraw.Draw(img)
-    draw.text((20, height - 30), 'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
-                                                                      , design.created_date), fill="white")
+            # se comenta la imagen con el nombre del diseñador y fecha
+            draw = ImageDraw.Draw(img)
+            draw.text((20, height - 30), 'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
+                                                                              , design.created_date), fill="white")
 
-    # Obtener nombre de archivo
-    file = '{}'.format(design.original_file)
-    position = file.index('.')
-    filename = file[9:position]
-    print(filename)
-    # Se guarda nueva imagen
-    status, created = State.objects.get_or_create(name='Disponible')
-    process_url = filename + '.png'
-    buffer = BytesIO()
-    img.save(buffer, format='PNG')
-    file_object = File(buffer)
-    file_object.content_type = 'image/png'
+            # Obtener nombre de archivo
+            file = '{}'.format(design.original_file)
+            position = file.index('.')
+            filename = file[9:position]
+            print(filename)
+            # Se guarda nueva imagen
+            status, created = State.objects.get_or_create(name='Disponible')
+            process_url = filename + '.png'
+            buffer = BytesIO()
+            img.save(buffer, format='PNG')
+            file_object = File(buffer)
+            file_object.content_type = 'image/png'
 
-    design.process_file.save(process_url, file_object)
-    design.state = status
-    design.save()
-    send_mail('Diseño procesado', 'Tu diseño ha sido procesado! Ahora es visible para todos', os.environ["EMAIL_DESIGN_USER"], [design.designer_email])
-    # Delete received message from queue
-    sqs.delete_message(
-         QueueUrl=queue_url,
-         ReceiptHandle=receipt_handle
-     )
+            design.process_file.save(process_url, file_object)
+            design.state = status
+            design.save()
+            send_mail('Diseño procesado', 'Tu diseño ha sido procesado! Ahora es visible para todos', os.environ["EMAIL_DESIGN_USER"], [design.designer_email])
+            # Delete received message from queue
+            sqs.delete_message(
+                 QueueUrl=queue_url,
+                 ReceiptHandle=receipt_handle
+             )
+        except KeyError:
+            print('no tiene mensaje')
     time.sleep(10)
 
 
