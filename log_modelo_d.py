de540fb299769026e8df0dcf2f964cf4d8cfc1b7
Nicolas Lema <n.lema@uniandes.edu.co>
2019-10-27 01:46:40 -0500

    Proceso que genera log del modelo D

---
+++ log_modelo_d.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+import psycopg2
+
+from django.conf import settings
+import time
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_1.settings")
+django.setup()
+from designs.models import Design
+
+while True:
+    contador = Design.objects.filter(process_file='').count()
+    f = open(settings.BASE_DIR + "/logs/log-modelo", "a+")
+    now = datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S-%f')[:-3]
+    f.write("Hora: %a -> Inicia conteo, cantidad de archivos por procesar: %a \r" % (
+        now, contador))
+    f.close()
+    time.sleep(10)
