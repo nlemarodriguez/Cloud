4abf0c45a6dd1a51571a77076a2031aa2fcddc16
Daury <daurylecca16@gmail.com>
2019-11-09 10:19:41 -0500

    Se ajusta configuración celery

--- procs.py
+++ procs.py
@@ -2,9 +2,10 @@ from celery import Celery
 from hirefire.procs.celery import CeleryProc
 from project_1 import settings
 
-#celery = Celery('project_1', broker=settings.BROKER_URL, backend=settings.BROKER_BACKEND+'://')
+celery = Celery('project_1', broker=settings.BROKER_URL, backend=settings.BROKER_BACKEND+'://')
 
 class WorkerProc(CeleryProc):
     name = 'worker'
     queues = ['celery']
- #   app = celery
+    simple_queues = True
+    app = celery
