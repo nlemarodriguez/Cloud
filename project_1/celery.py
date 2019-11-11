6a00f81a84c726dd65f5fa7dd9bb57308322db85
Daury <daurylecca16@gmail.com>
2019-11-11 01:51:39 -0500

    se ajusta celery

--- project_1/celery.py
+++ project_1/celery.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 import django
 from celery import Celery, task
+from kombu import Connection
+
 from project_1 import settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_1.settings")
 django.setup()
@@ -31,7 +33,7 @@ app.conf.beat_schedule = {
 
 
 @app.task(serializer='json', name='debug_task')
-def debug_task(self, message):
+def debug_task(self):
     # print('MENSAJE ES' + message)
     # return True
     # print('********************HELLO WORLD')
@@ -53,60 +55,67 @@ def debug_task(self, message):
     # except ClientError as e:
     #     response = None
 
-    receipt_handle = message['ReceiptHandle']
-    id = message['MessageAttributes']['Id']['StringValue']
-    design = Design.objects.get(id=int(id))
+    #receipt_handle = message['ReceiptHandle']
+    # id = message['MessageAttributes']['Id']['StringValue']
+    with Connection(settings.BROKER_URL) as conn:
+        simple_queue = conn.SimpleQueue('PRUEBA')
+        message = simple_queue.get(block=True, timeout=1)
+        print('Received: {0}'.format(message.payload))
+        message.ack()
+        simple_queue.close()
 
-    desired_size = 800
-    img = Image.open(design.original_file)
+        design = Design.objects.get(id=int(message.payload))
 
-    # Se ajusta tamaño de la imagen
-    old_size = img.size
-    ratio = float(desired_size) / max(old_size)
-    new_size = tuple([int(x * ratio) for x in old_size])
-    img = img.resize(new_size, Image.ANTIALIAS)
+        desired_size = 800
+        img = Image.open(design.original_file)
 
-    width, height = img.size
+        # Se ajusta tamaño de la imagen
+        old_size = img.size
+        ratio = float(desired_size) / max(old_size)
+        new_size = tuple([int(x * ratio) for x in old_size])
+        img = img.resize(new_size, Image.ANTIALIAS)
 
-    # se comenta la imagen con el nombre del diseñador y fecha
-    draw = ImageDraw.Draw(img)
-    draw.text((20, height - 30),
-              'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
-                                                   , design.created_date), fill="white")
+        width, height = img.size
 
-    # Obtener nombre de archivo
-    file = '{}'.format(design.original_file)
-    position = file.index('.')
-    filename = file[9:position]
-    print(filename)
-    # Se guarda nueva imagen
-    status, created = State.objects.get_or_create(name='Disponible')
-    process_url = filename + '.png'
-    buffer = BytesIO()
-    img.save(buffer, format='PNG')
-    file_object = File(buffer)
-    file_object.content_type = 'image/png'
+        # se comenta la imagen con el nombre del diseñador y fecha
+        draw = ImageDraw.Draw(img)
+        draw.text((20, height - 30),
+                  'Diseñador: {} {}\nFecha: {}'.format(design.designer_name, design.designer_last_name
+                                                       , design.created_date), fill="white")
 
-    design.process_file.save(process_url, file_object)
-    design.state = status
-    design.save()
+        # Obtener nombre de archivo
+        file = '{}'.format(design.original_file)
+        position = file.index('.')
+        filename = file[9:position]
+        print(filename)
+        # Se guarda nueva imagen
+        status, created = State.objects.get_or_create(name='Disponible')
+        process_url = filename + '.png'
+        buffer = BytesIO()
+        img.save(buffer, format='PNG')
+        file_object = File(buffer)
+        file_object.content_type = 'image/png'
 
-    message = Mail(
-        from_email=settings.EMAIL_DESIGN_USER,
-        to_emails=design.designer_email,
-        subject='Diseño procesado',
-        html_content='<p>Tu diseño ha sido procesado! <br><br> Ahora es visible para todos: '
-                     '<a href=http://entrega4-grupo01.herokuapp.com/empresas/' + design.project.company.url +
-                     '> Ver proyecto </a> </p>')
+        design.process_file.save(process_url, file_object)
+        design.state = status
+        design.save()
 
-    sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-    response = sg.send(message)
-    print(response.status_code, response.body, response.headers)
+        message = Mail(
+            from_email=settings.EMAIL_DESIGN_USER,
+            to_emails=design.designer_email,
+            subject='Diseño procesado',
+            html_content='<p>Tu diseño ha sido procesado! <br><br> Ahora es visible para todos: '
+                         '<a href=http://entrega4-grupo01.herokuapp.com/empresas/' + design.project.company.url +
+                         '> Ver proyecto </a> </p>')
 
-    # Delete received message from queue
-    sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
+        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+        response = sg.send(message)
+        print(response.status_code, response.body, response.headers)
+
+        # Delete received message from queue
+        # sqs.delete_message(
+        #     QueueUrl=queue_url,
+        #     ReceiptHandle=receipt_handle
+        # )
     return True
 
